37d07a67977306f920d4e00aa207da07636d22df
Jorge Aparicio <japaricious@gmail.com>
2015-01-26 16:05:07 -0500

    convert remaining `range(a, b)` to `a..b`

--- src/libcoretest/str.rs
+++ src/libcoretest/str.rs
@@ -17,8 +17,8 @@ fn test_bool_from_str() {
 
 fn check_contains_all_substrings(s: &str) {
     assert!(s.contains(""));
-    for i in range(0, s.len()) {
-        for j in range(i+1, s.len() + 1) {
+    for i in 0..s.len() {
+        for j in i+1..s.len() + 1 {
             assert!(s.contains(s.slice(i, j)));
         }
     }
